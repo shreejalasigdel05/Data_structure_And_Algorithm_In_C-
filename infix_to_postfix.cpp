6a38190a58a3d4dc2b5101d58ae8b478b929730d
shreejala <shreejalasigdel383@gmail.com>
2025-12-17 11:43:11 +0545

    infix to postfix

--- infix_to_postfix.cpp
+++ infix_to_postfix.cpp
@@ -11,3 +11,49 @@ int priority(char op)
     if(op=='+'||op=='-') return 1;
     return 0;
 }
+int main()
+{
+    char infix[100];
+    stack<char>st;
+    cout<<"Enter the infix expression: "<<endl;
+    cin>>infix;
+    int n=strlen(infix);
+    int i=0;
+    while(i<n)
+    {
+        char ch=infix[i];
+        if(isalnum(ch))
+        {
+            cout<<ch;
+        }
+        else if(ch=='(')
+        {
+            st.push(ch);
+        }
+        else if(ch==')')
+        {
+            while(!st.empty()&& st.top()!='(')
+            {
+                cout<<st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+        else
+        {
+            while(!st.empty()&&priority(ch)<=priority(st.top())&&st.top()!='(')
+            {
+                cout<<st.top();
+                st.pop();
+            }
+            st.push(ch);
+        }
+        i++;
+    }
+    while(!st.empty())
+    {
+        cout<<st.top();
+        st.pop();
+    }
+    cout<<endl;
+}
